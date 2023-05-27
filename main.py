40f309cacbe8c5ba7c047f740a0289344b8efa45
Clément Bazin de Jessey <cldejessey@hotmail.fr>
2023-05-27 15:25:44 +0200

    up app version

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pygame as pg
 from pygame._sdl2.video import Window, Renderer, Texture, Image
 import Exfont    
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 class App:
     def __init__(self):
