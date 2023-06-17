0946550a651e8fec517a460fa0293cca955bfcd1
Clément Bazin de Jessey <cldejessey@hotmail.fr>
2023-06-17 23:12:32 +0200

    add button pressed sound and effect

--- Sprites/Button.py
+++ Sprites/Button.py
@@ -1,10 +1,15 @@
 from .SpriteUnit import SpriteUnit
 from pygame._sdl2 import Image, Texture
+import pygame as pg
+import Settings
 
 class Button(SpriteUnit):
     def __init__(self, handler, image_unpressed, image_pressed, x, y, callback):
         self.image_unpressed = Image(Texture.from_surface(handler.renderer, image_unpressed))
-        self.image_pressed = Image(Texture.from_surface(handler.renderer, image_pressed))
+        if image_pressed == image_unpressed:
+            self.image_pressed = Image(Texture.from_surface(handler.renderer, (pg.transform.scale_by(image_pressed,0.5))))
+        else:
+            self.image_pressed  = Image(Texture.from_surface(handler.renderer, image_pressed))
         self.callback = callback
         self.pressed=False
         super().__init__(handler, self.image_unpressed, x, y)
@@ -18,8 +23,9 @@ class Button(SpriteUnit):
     def press(self):
         self.update_image(self.image_pressed)
         self.pressed=True
-        self.callback()
 
     def unpress(self):
+        pg.event.post(pg.event.Event(Settings.EVENT_SOUND, sound = 'button_press'))
         self.update_image(self.image_unpressed)
         self.pressed=False
+        self.callback()
