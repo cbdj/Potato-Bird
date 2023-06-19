6ad1cc64f9176953711c50b4cbe071308c2e7403
Clément Bazin de Jessey <cldejessey@hotmail.fr>
2023-06-19 22:26:43 +0200

    improve Smoke efficiency

--- Particles/Smoke.py
+++ Particles/Smoke.py
@@ -1,60 +1,54 @@
 import pygame as pg
 import random
 from pygame._sdl2 import Texture, Image, Renderer
-
-def scale(img: pg.Surface, factor):
-    w, h = img.get_width() * factor, img.get_height() * factor
-    return pg.transform.scale(img, (int(w), int(h)))
+from pygame import Rect
 
 class SmokeParticle:
-    def __init__(self, renderer, textures, x, y, vel_x, vel_y):
+    def __init__(self, renderer : Renderer, texture : Texture, alpha : int, x, y, vel_x, vel_y):
         self.x = x
         self.y = y
-        self.index_scale_k = 0
-        self.textures = textures
-        self.img = self.textures[0]
-        self.alpha = self.img.alpha
+        self.scale = 1.0
+        self.texture = texture
+        self.alpha = alpha
         self.alpha_rate = 3
         self.alive = True
         self.vy = vel_y
         self.vx = vel_x
         self.k = 0.01 * random.random() * random.choice([-1, 1])
+        self.src_rect = self.texture.get_rect()
+        self.dest_rect = self.src_rect.copy()
 
     def update(self):
         self.y += self.vy
         self.vy += self.k
         self.x -= self.vx
         self.vx *= 0.99
-        self.index_scale_k += 1
+        self.scale -= 0.01
         self.alpha -= self.alpha_rate
-        if self.alpha < 0:
+        if self.alpha <= 0 or self.scale <= 0:
             self.alpha = 0
+            self.scale = 0
             self.alive = False
         self.alpha_rate -= 0.1
         if self.alpha_rate < 1.5:
             self.alpha_rate = 1.5
-        self.img = self.textures[min(self.index_scale_k, len(self.textures))]
-        self.img.alpha = self.alpha
+        self.dest_rect.w = self.src_rect.w*self.scale
+        self.dest_rect.h = self.src_rect.h*self.scale
+        self.dest_rect.center = (self.x, self.y)
 
     def draw(self):
-        dest_rect = self.img.get_rect()
-        dest_rect.center=(self.x, self.y)
-        self.img.draw(self.img.get_rect(), dest_rect)
-
-
+        self.texture.alpha = self.alpha
+        self.texture.draw(self.src_rect, self.dest_rect)
 
 class Smoke:
     def __init__(self, renderer, surface, x, y):
-        self.textures = [Image(Texture.from_surface(renderer, scale(surface, factor/1000))) for factor in reversed(range(100,1000,5))]
-        for texture in self.textures:
-            texture.alpha = surface.get_alpha()
+        self.texture = Texture.from_surface(renderer, surface)
+        self.alpha = surface.get_alpha()
         self.x = x
         self.y = y
         self.vel_x = 0
         self.renderer = renderer
-        self.surface = surface
         self.particles = []
-        self.frames = 0
 
     def set_source_position(self,x,y):
         self.x = x
@@ -66,12 +60,10 @@ class Smoke:
 
     def update(self):
         self.particles = [i for i in self.particles if i.alive]
-        self.frames += 1
-        if self.frames % 2 == 0 and (self.vel_x or self.vel_y):
-            self.particles.append(SmokeParticle(self.renderer,self.textures,self.x,self.y, self.vel_x, self.vel_y))
-            self.frames = 0
-        for i in self.particles:
-            i.update()
+        if self.vel_x or self.vel_y:
+            self.particles.append(SmokeParticle(self.renderer,self.texture,self.alpha, self.x,self.y, self.vel_x, self.vel_y))
+        for particle in self.particles:
+            particle.update()
 
     def draw(self):
         for particle in self.particles:
